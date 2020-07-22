fdf6ddfe2583d271703f4bada20416632867a8a0
Patricia Della Matta <patricia_dellamatta@hotmail.com>
2020-07-22 22:48:15 +0100

    Configurando o JWT na API

--- src/services/GoShopping.Identity.API/Startup.cs
+++ src/services/GoShopping.Identity.API/Startup.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using GoShopping.Identity.API.Data;
+using GoShopping.Identity.API.Extensions;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 namespace GoShopping.Identity.API
@@ -38,11 +42,39 @@ namespace GoShopping.Identity.API
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
 
+            // Usar Json Web Token
+            var appSettingsSection = Configuration.GetSection("AppSettings");
+            services.Configure<AppSettings>(appSettingsSection);
+
+            var appSettings = appSettingsSection.Get<AppSettings>();
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(bearerOptions =>
+            {
+                bearerOptions.RequireHttpsMetadata = true;
+                bearerOptions.SaveToken = true;
+                bearerOptions.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidAudience = appSettings.ValidIn,
+                    ValidIssuer = appSettings.Issuer
+                };
+            });
+
             services.AddControllers();
 
             //Swagger
-            services.AddSwaggerGen(c => {
-                c.SwaggerDoc("v1", new OpenApiInfo {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
                     Title = "GoShopping Enterprise Identity API",
                     Description = "Esta API foi gerada conforme o curso ASP.NET Core Enterprise Applications.",
                     Contact = new OpenApiContact() { Name = "Patricia Matta", Email = "contato@goshopping.br" },
@@ -71,7 +103,7 @@ namespace GoShopping.Identity.API
 
             // Roteamento
             app.UseRouting();
-            
+
             app.UseAuthentication();
             app.UseAuthorization();
 
